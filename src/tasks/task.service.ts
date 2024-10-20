bf68b881fc616d901c677ac260c7ef7dedbf700f
npcnon <afkmhafric@gmail.com>
2024-10-20 23:31:36 +0800

    removed api fetch(to make render sleep)

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -7,11 +7,5 @@ export class TaskService {
   @Cron('0 * * * * *') // Runs every minute
   async handleCron() {
     console.log('Task run at:', new Date().toISOString());
-    try {
-      const response = await axios.get('https://node-mysql-signup-verification-api.onrender.com/external/get-campus-active');
-      console.log('Fetched data:', response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error.message);
-    }
-  }
+
 }
